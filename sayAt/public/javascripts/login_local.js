2b5cf1a656661add97118699b4109619a867e53d
Nishad Fowzan <ahmednishad3@gmail.com>
2019-05-04 21:12:37 +0530

    Sayat and listing functionality added. Logging and commenting to be added

---
+++ sayAt/public/javascripts/login_local.js
@@ -0,0 +1,8 @@
+let user = document.querySelector('#user'),
+    email = document.querySelector('#email'),
+    password = document.querySelector('#password'),
+    submit = document.querySelector("[type='submit'")
+
+submit.addEventListener('click', ()=>{
+    console.log('Adding user')
+})
